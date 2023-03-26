ecfdfca6117a81e40cf57b5976061888bf6cc027
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-26 19:31:56 +0530

    completed basic settings
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
-  return (
-    <div>
-      header
-    </div>
-  )
-}
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lastScrollY, setLastScrollY] = useState(0);
+  return <div>header</div>;
+};
 
-export default Header
+export default Header;
