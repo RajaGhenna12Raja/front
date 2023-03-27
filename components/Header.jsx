f31901a70f9d7bc191e997550d3879fb6e87ce05
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-27 08:47:19 +0530

    completed MenuSection
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState, useEffect } from 'react';
 import Wrapper from './Wrapper';
 import Image from 'next/image';
@@ -15,6 +14,27 @@ const Header = () => {
   const [showCatMenu, setShowCatMenu] = useState(false);
   const [show, setShow] = useState('translate-y-0');
   const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow('-translate-y-[80px]');
+      } else {
+        setShow('shadow-sm');
+      }
+    } else {
+      setShow('translate-y-0');
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+  }, [lastScrollY]);
+
   return (
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform-300 ${show}`}
@@ -42,7 +62,7 @@ const Header = () => {
           {/* ICON-STARTS */}
           <div className='w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative'>
             <IoMdHeartEmpty className='text-[19px] md:text-[24px]' />
-            <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-2 md:px-5'>
+            <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]'>
               51
             </div>
           </div>
@@ -51,7 +71,7 @@ const Header = () => {
           {/* ICON-STARTS */}
           <div className='w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative'>
             <BsCart className='text-[15px] md:text-[20px]' />
-            <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-2 md:px-5'>
+            <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]'>
               5
             </div>
           </div>
