20328513256e16d2f12f3cd7d2dc1c639000d06e
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-28 17:25:12 +0530

    installed ui part
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -16,7 +16,60 @@ const CartItem = () => {
       </div>
       {/* image ends */}
 
-      <div className='w-full flex flex-col'></div>
+      <div className='w-full flex flex-col'>
+        <div className='flex flex-col md:flex-row justify-between'>
+          {/* product tittle */}
+          <div className='text-lg md:text-2xl font-semibold text-black/[0.8]'>
+            Jordan Retro 6 G
+          </div>
+          {/* Product SubTitle */}
+          <div className='text-sm md:text-md font-medium text-black/[0.5] block md:hidden'>
+            Men&apos;s Golf Shoes
+          </div>
+          {/* Product Price */}
+          <div className='text-sm md:text-md font-bold text-black/[0.5] mt-2'>
+            MRP: Rs.19695.00
+          </div>
+        </div>
+        {/* Product SubTitle */}
+        <div className='text-md font-medium text-black/[0.5] hidden md:block'>
+          Men&apos;s Golf Shoes
+        </div>
+        {/* selector size starts */}
+        <div className='flex items-center justify-between mt-4'>
+          <div className='flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md'>
+            <div className='flex items-center gap-1'>
+              <div className='font-semibold'>Size</div>
+              <select className='hover:text-black'>
+                <option value='1'>UK 6</option>
+                <option value='2'>UK 7</option>
+                <option value='3'>UK 8</option>
+                <option value='4'>UK 9</option>
+                <option value='5'>UK 10</option>
+                <option value='6'>UK 10.5</option>
+                <option value='7'>UK 11</option>
+              </select>
+            </div>
+
+            <div className='flex items-center gap-1'>
+              <div className='font-semibold'>Quantity</div>
+              <select className='hover:text-black'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+                <option value='8'>8</option>
+                <option value='9'>9</option>
+                <option value='10'>10</option>
+              </select>
+            </div>
+          </div>
+          <RiDeleteBin6Line className='cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]'/>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -29,10 +29,53 @@ const Cart = () => {
           {/* summary starts */}
           <div className='flex-[1]'>
             <div className='text-lg font-bold'>Summary</div>
+            <div className='p-5 my-5 bg-black/[0.05] rounded-xl'>
+              <div className='flex justify-between'>
+                <div className='uppercase text-md md:text-lg font-medium text-black'>
+                  SubTotal
+                </div>
+                <div className='text-md md:text-lg font-medium text-black'>
+                  Rs. 19695.00
+                </div>
+              </div>
+              <div className='text-sm md:text-md py-5 border-t mt-5'>
+                Dummy data is mock data generated at random as a substitute for
+                live data in testing environments. In other words, dummy data
+                acts as a placeholder for live ...
+              </div>
+            </div>
+            {/* button starts checkout */}
+            <button className='w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75'>
+              CheckOut
+            </button>
+            {/* button checkout emds */}
           </div>
           {/* summary ends */}
         </div>
         {/* cart content ends */}
+
+        {/* this is empty screen */}
+
+        {/* <div className='flex-[2] flex flex-col items-center pb-[50px] md:-mt-14'>
+          <Image
+            src='/empty-cart.jpg'
+            height={100}
+            width={100}
+            alt='Empty Cart'
+          />
+          <span className='text-lg font-bold'>Your Cart is Empty</span>
+          <span className='text-center mt-4'>
+            Looks like you have not added anything in your cart
+            <br />
+            Go ahead and Explore top categories
+          </span>
+          <Link
+            className='py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3'
+            href='/'
+          >
+            Continue Shopping
+          </Link>
+        </div> */}
       </Wrapper>
     </div>
   );
